ec60eb9363ba3f52018dc0efb2177040ca32055a
renaldikristian <renaldi.kristian@ti.ukdw.ac.id>
2023-12-19 17:58:56 +0700

    Menambahkan Rating Bintang

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import Searching from './components/Searching';
-import Filter from './components/Filter';
-import RatingStars from './components/RatingStars';
 import './App.css';
 import ReactPaginate from 'react-paginate';
 
@@ -23,7 +20,7 @@ const Modal = ({ isOpen, onClose, children }) => {
   );
 };
 
-function App({ title, rating }) {
+function App() {
   const [movie, setMovie] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
   const [showPrevious, setShowPrevious] = useState(false);
@@ -34,18 +31,48 @@ function App({ title, rating }) {
   const [name, setName] = useState('');
   const [showForm, setShowForm] = useState(true);
   const [modalOpen, setModalOpen] = useState(false);
+  const [selectedStars, setSelectedStars] = useState([]);
+  const [movieRatings, setMovieRatings] = useState({});
+
+  const handleStarClick = (index) => {
+    // Lakukan sesuatu ketika bintang diklik
+    if (!selectedMovie) return;
+
+    const movieId = selectedMovie.id;
+    const isSelected = movieRatings[movieId]?.includes(index);
+
+    setMovieRatings((prevRatings) => {
+      const prevMovieRatings = prevRatings[movieId] || [];
+      const updatedRatings = isSelected
+        ? prevMovieRatings.filter((rating) => rating !== index)
+        : [...prevMovieRatings, index];
+
+      return { ...prevRatings, [movieId]: updatedRatings };
+    });
+
+    // Simpan data ke localStorage
+    localStorage.setItem('selectedStars', JSON.stringify(selectedStars));
+  };
 
   const fetchMovie = async () => {
-
-    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=27bd4dad0754c77391111e35f827bd6a&page=7`)
+    console.log(movie);
+    fetch('https://api.themoviedb.org/3/discover/movie?api_key=27bd4dad0754c77391111e35f827bd6a')
       .then(response => response.json())
       .then(json => setMovie(json.results))
   }
   useEffect(() => {
-    fetchMovie()
-  }, [])
+    fetchMovie();
+    const storedRatings = JSON.parse(localStorage.getItem('movieRatings'));
+    if (storedRatings) {
+      setMovieRatings(storedRatings);
+    }
+  }, []);
   const handleOpenModal = () => {
     setModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalOpen(false);
   };
 
   const search = async (q) => {
@@ -102,11 +129,44 @@ function App({ title, rating }) {
   }
 
   const refreshPage = async () => {
+    localStorage.setItem('movieRatings', JSON.stringify(movieRatings));
     window.location = document.referrer;
-  }
+  };
   return (
     <>
-      <Searching showForm={showForm} search={search} />
+      <form
+        action=""
+        className="search-bar"
+        style={{ display: showForm ? '' : 'none' }}
+        onSubmit={(e) => {
+          e.preventDefault();
+          const searchValue = e.target.querySelector('.MovieSearch').value;
+          search(searchValue);
+        }}
+      >
+        <input
+          placeholder='Enter Film..'
+          className='MovieSearch'
+        />
+        <button className="search-btn" type="submit">
+          <span>Search</span>
+        </button>
+        <div className='container-category'>
+          <input className="dark-light" type="checkbox" id="dark-light" name="dark-light" />
+
+          <div className="light-back"></div>
+
+          <div className="sec-center">
+            <input className="dropdown" type="checkbox" id="dropdown" name="dropdown" />
+            <label className="for-dropdown" htmlFor="dropdown">Filter By<i className="uil uil-arrow-down"></i></label>
+            <div className="section-dropdown">
+              <a>Rating Tertinggi<i className="uil uil-arrow-right"></i></a>
+              <a>Popular<i className="uil uil-arrow-right"></i></a>
+              <a>Upcoming<i className="uil uil-arrow-right"></i></a>
+            </div>
+          </div>
+        </div>
+      </form>
 
       <>
         {selectedMovie !== null ? (
@@ -121,7 +181,19 @@ function App({ title, rating }) {
                 <p>Release Date : {selectedMovie.release_date}</p>
                 <p>Duration: {(selectedMovie.runtime - (selectedMovie.runtime % 60)) / 60} Jam {selectedMovie.runtime % 60} Menit</p>
                 <p>Status: {selectedMovie.status}</p>
-                <RatingStars rating={selectedMovie.vote_average} />
+                <div className="container">
+                  <div className="container__items">
+                    {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((index) => (
+                      <div
+                        key={index}
+                        className={`star-stroke ${movieRatings[selectedMovie.id]?.includes(index) ? 'selected' : ''}`}
+                        onClick={() => handleStarClick(index)}
+                      >
+                        <div className="star-fill">&#9733;</div>
+                      </div>
+                    ))}
+                  </div>
+                </div>
                 <button className='button-5' onClick={refreshPage}>Kembali</button>
 
                 {comments.length > 0 && (
@@ -179,7 +251,6 @@ function App({ title, rating }) {
         ) : (
 
           <div className="card-container">
-            <Filter setMovie={setMovie} />
             {/* Tampilkan daftar gambar */}
             {movie.map((val, index) => (
               <div className="card" key={index}>
